4453fba409b03dab1ebc51590206a6fe95620fb2
yo <yohanhxh@gmail.com>
2023-08-02 16:31:08 -0300

    Fix: compile errors on 1030 and implements a more performatic strategie

--- beecrowd/_1030.rs
+++ beecrowd/_1030.rs
@@ -1,4 +1,4 @@
-pub const fn input() -> String {
+pub fn input() -> String {
     let mut input_string: String = String::new();
     std::io::stdin()
         .read_line(&mut input_string)
@@ -7,16 +7,7 @@ pub const fn input() -> String {
     input_string
 }
 
-pub fn parse_to_signed_int(str: &String) -> isize {
-    let parsed_value: isize = str
-        .trim()
-        .parse()
-        .expect("Panicked while parsing string");
-
-    parsed_value
-}
-
-pub const fn parse_to_unsigned_int(str: String) -> usize {
+pub fn parse_to_unsigned_int(str: &str) -> usize {
     let parsed_value: usize = str
         .trim()
         .parse()
@@ -25,49 +16,48 @@ pub const fn parse_to_unsigned_int(str: String) -> usize {
     parsed_value
 }
 
-struct Input {
-    jump_size: usize,
-    circle_size: usize,
-}
-
 pub fn run() {
-    const INPUTS_AMOUNT: usize = parse_to_unsigned_int(input());
-    let mut inputs: [Input; INPUTS_AMOUNT];
-    
-    for i in 0..=(INPUTS_AMOUNT-1) {
-        let mut user_input_string: String = input();
+    let inputs_amount: usize = parse_to_unsigned_int(&input());
+    let mut inputs: Vec<Input> = Vec::new();
 
-        let mut current_circle_size: usize;
-        let mut current_jump_size: usize;
+    for i in 0..=(inputs_amount-1) {
+        let user_input_string: String = input();
 
-        let mut counter = 0;
-        for i in user_input_string.split(" ") {
-            match i.parse::<usize>() {
+        let mut current_circle_size: usize = 0;
+        let mut current_jump_size: usize = 0;
+
+        let mut counter: usize = 0;
+        for j in user_input_string.trim().split(" ") {
+            match j.parse::<usize>() {
                 Ok(number) => {
                     if counter == 0 {
                         current_circle_size = number
                     } else {
                         current_jump_size = number
                     }
+                    counter += 1;
                 },
                 Err(_) => println!("Invalid numeric string"),
             }
         }
 
-        inputs[i] = Input {
+        inputs.insert(i, Input {
             jump_size: current_jump_size,
             circle_size: current_circle_size
-        }
+        })
     }
+    for i in 1..=inputs_amount {
+        println!("Case {}: {}", i, run_killing_spree_v2(inputs[i-1].jump_size, inputs[i-1].circle_size));
+    }
+}
 
-    for i in 1..=INPUTS_AMOUNT {
-        println!("Case {}: {}", i, run_killing_spree());
-    }
+struct Input {
+    jump_size: usize,
+    circle_size: usize,
 }
 
 struct Person {
     alive: bool,
-    position: usize,
 }
 
 impl Person {
@@ -75,58 +65,79 @@ impl Person {
         self.alive
     }
 
-    fn get_next_position(&self) -> usize {
-        self.position + 1
-    }
-
-    fn kill(&self) {
+    fn kill(&mut self) {
         self.alive = false;
     }
 }
 
+
+
 fn create_circle(size: &usize) -> Vec<Person> {
     let mut circle = Vec::new();
-    for i in 0..=(size-1) {
+    for _i in 0..=(size-1) {
         circle.push(Person {
             alive: true,
-            position: i,
         })
     }
     circle
 }
 
-fn get_next_alive_position_from(position: &usize, circle: &[Person]) -> usize {
-    for i in position..=position+circle.len()-1 {
-        if circle[i%circle.len()].alive {
-            i%circle.len()
+fn get_next_alive_position_from(position: usize, circle: &[Person]) -> usize {
+    for i in position+1..=position+circle.len() {
+        if circle[(i)%circle.len()].alive {
+            return i%circle.len();
         }
     }
     panic!("Could not find the next alive");
 }
 
-fn find_next_target_position(current_killing_position: usize, jump_size: usize, circle: &[Person]) {
-    let mut current_guess: usize = (current_killing_position + jump_size)%circle.len();
-    while circle[current_guess].is_alive() {
-        current_guess = (current_guess + jump_size)%circle.len();
+fn find_next_target_position(current_killing_position: usize, jump_size: usize, circle: &Vec<Person>) -> usize {
+    let mut current_guess: usize = current_killing_position;
+
+    for _i in 1..=jump_size {
+        current_guess = get_next_alive_position_from(current_guess, &circle);
     }
-    (current_guess)
+    current_guess
 }
 
-fn find_last_alive_position(circle: &[Person]) {
-    for i in ..=(circle.len()-1) {
+fn find_last_alive_position(circle: &[Person]) -> usize {
+    for i in 0..=(circle.len()-1) {
         if circle[i].is_alive() {
-            i
+            return i
         }
     }
+    panic!("Could not find last alive");
 }
 
-fn run_killing_spree(jump_size: &usize, circle: [Person]) {
+fn run_killing_spree(jump_size: usize, circle_size: usize) -> usize {
     let mut dead_amount: usize = 0;
     let mut killing_position: usize = 0;
-    while dead_amount != (circle_size.len()-1) {
-        killing_position = find_next_target_position();
+    let mut circle: Vec<Person> = create_circle(&circle_size);
+
+    while dead_amount != (circle.len()-1) {
+        killing_position = find_next_target_position(killing_position, jump_size, &circle);
         circle[killing_position].kill();
         dead_amount+=1;
     }
-    find_last_alive_position()
+    find_last_alive_position(&circle)
+}
+
+
+fn run_killing_spree_v2(jump_size: usize, circle_size: usize) -> usize {
+    // this version implements a MUCH simpler algorithm, runs faster, has fewer operations and uses less memory
+    let mut people: Vec<usize> = Vec::new();
+
+    for i in 0..=circle_size-1 {
+        people.push(i);
+    }
+
+    let mut killed_amount: usize = 0;
+    let mut current_index: usize = 0;
+
+    while killed_amount != circle_size-1 {
+        current_index = ((current_index + jump_size)-1)%people.len();
+        people.remove(current_index);
+        killed_amount += 1;
+    }
+    people[0]+1
 }
--- main.rs
+++ main.rs
@@ -1,9 +1,7 @@
 mod beecrowd {
-    pub mod _1066;
     pub mod _1030;
 }
 
 fn main() {
-    // beecrowd::_1030::run();
-
+    beecrowd::_1030::run();
 }
